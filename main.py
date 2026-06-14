7eac85dafc87f89aa5565fb2896b5541b82a1349
Hamza El Achhab <HamzaElAchhab99@gmail.com>
2026-06-14 00:25:34 +0000

    wd

--- main.py
+++ main.py
@@ -1,62 +1,101 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
+
 model_name = "Qwen/Qwen3-0.6B"
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+#install the tokenizer for the type of model name
+tokenizer = AutoTokenizer.from_pretrained(
+    "Qwen/Qwen3-0.6B",
+    trust_remote_code=True
+)
+
+
+#install the model
 model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    torch_dtype=torch.float16,
-    device_map="auto")
-
-
-
-
-
-# prompt = "explain to ne what is AI?"
-
-# inputs = tokenizer(prompt, return_tensors="pt")
-
-# outputs = model.generate(inputs["input_ids"], max_new_tokens=50)
-
-
-# print("\n\n\n")
-# print(tokenizer.decode(outputs[0]))
+    "Qwen/Qwen3-0.6B",
+    dtype=torch.float16,
+    device_map="auto"
+)
 
 
 
 
 
 context = """
-Artificial Intelligence (AI) is a field of computer science that focuses on building systems
-capable of performing tasks that normally require human intelligence, such as learning,
-reasoning, and problem-solving. AI is used in education, healthcare, and industry.
+the small baby's name is thrthng.
 """
+question = "What is the name of small baby?"
 
 
 
-question = "What is AI used for?"
+# prompt = f"""
+# Context:
+# {context}
 
-prompt = f"""
-Context:
-{context}
-Question:
-{question}
-Answer ONLY using the context:
-"""
+# Question:
+# {question}
+
+# Instruction:
+# - Answer ONLY from the context.
+# - Do NOT add extra information.
+# - Give a short direct answer only.
+# """
 
 
 
+model.eval()
 
-inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
 
-outputs = model.generate(
-    **inputs,
-    max_new_tokens=100,
-    temperature=0.2
+
+# use obj as fun cause this -> : __call__
+inputs = tokenizer(
+    question,
+    return_tensors="pt",
+    truncation=True,
+    max_length=4096,
+    padding=False,
+    return_attention_mask=True
 )
 
 
-print()
-print()
-print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+print(inputs)
+
+
+# outputs = model.generate(
+#     **tokens,
+#     max_new_tokens=50,
+#     temperature=0.2
+# )
+
+
+
+# print()
+# print()
+# print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+
+
+
+
+
+
+# prompt = f"""
+# You are answering questions about the vLLM codebase.
+
+# Use ONLY the provided context.
+
+# If the answer is not present in the context,
+# reply exactly:
+
+# I could not find the answer in the retrieved sources.
+
+# Context:
+# {context}
+
+# Question:
+# {question}
+
+# Answer:
+# """
